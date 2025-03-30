941ea49be74f4631e7897d900b75ff62f5064ac3
Just a C++ programmer... <patezinhoverde@gmail.com>
2025-03-30 12:53:15 -0300

    Create Main.cpp

---
+++ src/Main.cpp
@@ -0,0 +1,12 @@
+// Importations:
+#include <cpputils.hpp>
+#include <iostream>
+
+
+// Main code:
+int main()
+{
+  std::cout << bold << "Hello, World!" << base_terminal << std::endl;
+
+  return 0;
+}
